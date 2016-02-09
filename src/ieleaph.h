2d7b6675aa17602e555644d4769ffbd19fb31c1b
Spiek <spiek@ymail.com>
2016-02-09 11:10:41 +0100

    Improving * make IEleaph::sendDataPacket functions dynamic

--- src/ieleaph.h
+++ src/ieleaph.h
@@ -110,8 +110,8 @@ class IEleaph : public QObject
         bool startTcpListening(quint16 port, QHostAddress address = QHostAddress::Any, bool keepConnectedHostsAlive = true, bool useSSL = false, QString pathCrt = "", QString pathKey = "", bool verifyPeer = true);
 
         // static datapacket send functions
-        static void sendDataPacket(QIODevice* device, QByteArray *baDatatoSend);
-        static void sendDataPacket(QIODevice* device, std::string strDatatoSend);
+        void sendDataPacket(QIODevice* device, QByteArray *baDatatoSend);
+        void sendDataPacket(QIODevice* device, std::string strDatatoSend);
 
     public slots:
         void addDevice(QIODevice* device, IEleaph::DeviceForgetOptions forgetoptions = IEleaph::ForgetDeviceOnClose, bool enableKeepAliveSystem = true, uint keepAlivePingTime = 1000, uint keepAliveCloseTimeoutTime = 30000);
